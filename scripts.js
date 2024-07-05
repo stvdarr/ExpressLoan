d4c7de85fc7a0b2834c270c770d655bfde3c23d3
StvDarr <stevendarren21375@gmail.com>
2024-07-05 15:09:29 +0700

    App download QR tooltip, TKB popover, Header carousel, Color revision

--- scripts.js
+++ scripts.js
@@ -2,14 +2,20 @@ const nav = document.getElementsByTagName('nav')[0];
 
 window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight) {
-        nav.style.backdropFilter = 'blur(10px)';
-        nav.style.webkitBackdropFilter = 'blur(10px)';
-        document.querySelectorAll('.nav-link').forEach(el => el.style.cssText = 'color: black !important');
-        document.querySelectorAll('.navbar-brand').forEach(el => el.style.color = 'black');
+        nav.style.backdropFilter = 'blur(2px)';
+        nav.style.webkitBackdropFilter = 'blur(2px)';
+        // document.querySelectorAll('.nav-link').forEach(el => el.style.cssText = 'color: black !important');
+        // document.querySelectorAll('.navbar-brand').forEach(el => el.style.color = 'black');
         return;
     }
-    document.querySelectorAll('.nav-link').forEach(el => el.style.cssText = 'color: white !important');
-    document.querySelectorAll('.navbar-brand').forEach(el => el.style.color = 'white');
-    nav.style.backdropFilter = 'blur(0)';
-    nav.style.webkitBackdropFilter = 'blur(0)'; 
+    // document.querySelectorAll('.nav-link').forEach(el => el.style.cssText = 'color: white !important');
+    // document.querySelectorAll('.navbar-brand').forEach(el => el.style.color = 'white');
+    nav.style.backdropFilter = 'blur(2px)';
+    nav.style.webkitBackdropFilter = 'blur(2px)'; 
 });
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
